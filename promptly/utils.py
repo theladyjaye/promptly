c6cf5289d53015c05be2ac3497de5e2bea49276b
Adam Venturella <aventurella@gmail.com>
2013-10-16 05:11:36 -0700

    python 3 compat update

--- promptly/utils.py
+++ promptly/utils.py
@@ -2,5 +2,6 @@ def numeric_options(options):
 
     max = len(options) + 1
     numbers = range(1, max)
-    return zip(numbers, options)
+    # call to list if for python 3
+    return list(zip(numbers, options))
 
