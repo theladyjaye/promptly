5fef7326780ff92db1b7814ceb9c138692e1e43e
Adam Venturella <aventurella@gmail.com>
2013-10-28 19:22:39 -0700

    added python 2.6

--- setup.py
+++ setup.py
@@ -36,6 +36,7 @@ setup(
         'Natural Language :: English',
         'License :: OSI Approved :: ISC License (ISCL)',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.1',
         'Programming Language :: Python :: 3.2',
